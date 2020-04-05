de2814633e2981e7d97c4c569e7f67d87d0850e2
John Morsley <johnmorsley@gmail.com>
2020-04-05 22:53:18 +0100

    .

--- scripts/install_concourse.sh
+++ scripts/install_concourse.sh
@@ -22,22 +22,24 @@ chmod 400 ${FOLDER}/*.pem
   
 # Concourse...
 
-#kubectl apply --filename $(pwd)/k8s/worker-storage-class.yaml
-#kubectl apply --filename $(pwd)/$FOLDER/worker-persistent-volume-0.yaml
-#kubectl apply --filename $(pwd)/$FOLDER/worker-persistent-volume-1.yaml
+kubectl apply --filename $(pwd)/k8s/worker-storage-class.yaml
+kubectl apply --filename $(pwd)/${FOLDER}/worker-persistent-volume-0.yaml
+kubectl apply --filename $(pwd)/${FOLDER}/worker-persistent-volume-1.yaml
 
-#kubectl apply --filename $(pwd)/k8s/postgresql-storage-classs.yaml
-#kubectl apply --filename $(pwd)/$FOLDER/postgresql-persistent-volume-0.yaml
+kubectl apply --filename $(pwd)/k8s/postgresql-storage-classs.yaml
+kubectl apply --filename $(pwd)/${FOLDER}/postgresql-persistent-volume-0.yaml
 
-#helm repo add concourse https://concourse-charts.storage.googleapis.com/
+helm repo add concourse https://concourse-charts.storage.googleapis.com/
 
 #helm repo update
 
-#kubectl create namespace $NAMESPACE
+kubectl create namespace ${NAMESPACE}
 
-#helm install $NAME concourse/concourse --values $(pwd)/k8s/concourse-values.yaml --namespace $NAMESPACE
+helm install ${NAME} concourse/concourse \
+  --values $(pwd)/k8s/concourse-values.yaml \
+  --namespace ${NAMESPACE}
 
-#kubectl --namespace $NAMESPACE rollout status $NAME
+#kubectl --namespace ${NAMESPACE} rollout status ${NAME}
 
 # https://whynopadlock.com
 # https://www.ssllabs.com/ssltest/
