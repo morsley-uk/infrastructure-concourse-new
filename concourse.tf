e9a800e99ae8fb164698043f4672d5d17a43c41b
John Morsley <johnmorsley@gmail.com>
2020-04-10 14:52:03 +0100

    .

--- concourse.tf
+++ concourse.tf
@@ -31,190 +31,155 @@ module "kubernetes-cluster" {
 }
 
 # Concourse needs 2 AWS EBS...
-resource "aws_ebs_volume" "worker-ebs" {
-
-  availability_zone = var.availability_zone
-  size              = var.worker_storage_size
-
-  tags = {
-    Name = "worker-storage-ebs"
-  }
-
-}
-
-resource "local_file" "worker-persistent-volume-0-yaml" {
-
-  content  = templatefile("${path.cwd}/k8s/worker-persistent-volume-0.yaml", { VOLUME_ID = aws_ebs_volume.worker-ebs.id })
-  filename = "${local.folder}/worker-persistent-volume-0.yaml"
-
-}
-
-resource "local_file" "worker-persistent-volume-1-yaml" {
-
-  content  = templatefile("${path.cwd}/k8s/worker-persistent-volume-1.yaml", { VOLUME_ID = aws_ebs_volume.worker-ebs.id })
-  filename = "${local.folder}/worker-persistent-volume-1.yaml"
-
-}
-
-resource "aws_ebs_volume" "postgresql-ebs" {
-
-  availability_zone = var.availability_zone
-  size              = var.postgresql_storage_size
-
-  tags = {
-    Name = "postgresql-storage-ebs"
-  }
-
-}
-
-resource "local_file" "postgresql-persistent-volume-0-yaml" {
-
-  content  = templatefile("${path.cwd}/k8s/postgresql-persistent-volume-0.yaml", { VOLUME_ID = aws_ebs_volume.postgresql-ebs.id })
-  filename = "${local.folder}/postgresql-persistent-volume-0.yaml"
-
-}
-
-# Install Istio...
-//resource "null_resource" "install-istio" {
+//resource "aws_ebs_volume" "worker-ebs" {
 //
-//  depends_on = [
-//    module.kubernetes-cluster
-//  ]
-//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//  availability_zone = var.availability_zone
+//  size              = var.worker_storage_size
 //
-//  provisioner "local-exec" {
-//    command = "chmod +x scripts/install_istio.sh && bash scripts/install_istio.sh"
-//    environment = {
-//      FOLDER = local.folder
-//    }
+//  tags = {
+//    Name = "worker-storage-ebs"
 //  }
 //
 //}
-
-# Is Istio ready...?
-//resource "null_resource" "is-istio-ready" {
 //
-//  depends_on = [
-//    null_resource.install-istio
-//  ]
+//resource "local_file" "worker-persistent-volume-0-yaml" {
 //
-//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//  content  = templatefile("${path.cwd}/k8s/worker-persistent-volume-0.yaml", { VOLUME_ID = aws_ebs_volume.worker-ebs.id })
+//  filename = "${local.folder}/worker-persistent-volume-0.yaml"
 //
-//  provisioner "local-exec" {
-//    command = "chmod +x scripts/is_istio_ready.sh && bash scripts/is_istio_ready.sh"
-//    environment = {
-//      FOLDER = local.folder
-//    }
+//}
+//
+//resource "local_file" "worker-persistent-volume-1-yaml" {
+//
+//  content  = templatefile("${path.cwd}/k8s/worker-persistent-volume-1.yaml", { VOLUME_ID = aws_ebs_volume.worker-ebs.id })
+//  filename = "${local.folder}/worker-persistent-volume-1.yaml"
+//
+//}
+//
+//resource "aws_ebs_volume" "postgresql-ebs" {
+//
+//  availability_zone = var.availability_zone
+//  size              = var.postgresql_storage_size
+//
+//  tags = {
+//    Name = "postgresql-storage-ebs"
 //  }
+//
+//}
+//
+//resource "local_file" "postgresql-persistent-volume-0-yaml" {
+//
+//  content  = templatefile("${path.cwd}/k8s/postgresql-persistent-volume-0.yaml", { VOLUME_ID = aws_ebs_volume.postgresql-ebs.id })
+//  filename = "${local.folder}/postgresql-persistent-volume-0.yaml"
 //
 //}
 
 # Using Helm install Concourse on the previously created Kubernetes cluster...
-resource "null_resource" "install-concourse" {
-
-  depends_on = [
-    aws_ebs_volume.worker-ebs,
-    aws_ebs_volume.postgresql-ebs,
-    module.kubernetes-cluster #,
-    #null_resource.is-istio-ready
-  ]
-
-  # https://www.terraform.io/docs/provisioners/local-exec.html
-
-  provisioner "local-exec" {
-    command = "chmod +x ${path.module}/scripts/install_concourse.sh && bash ${path.module}/scripts/install_concourse.sh"
-    environment = {
-      FOLDER          = local.folder
-      DEPLOYMENT_NAME = var.deployment_name
-      NAMESPACE       = var.namespace
-    }
-  }
-
-}
+//resource "null_resource" "install-concourse" {
+//
+//  depends_on = [
+//    aws_ebs_volume.worker-ebs,
+//    aws_ebs_volume.postgresql-ebs,
+//    module.kubernetes-cluster #,
+//    #null_resource.is-istio-ready
+//  ]
+//
+//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//
+//  provisioner "local-exec" {
+//    command = "chmod +x ${path.module}/scripts/install_concourse.sh && bash ${path.module}/scripts/install_concourse.sh"
+//    environment = {
+//      FOLDER          = local.folder
+//      DEPLOYMENT_NAME = var.deployment_name
+//      NAMESPACE       = var.namespace
+//    }
+//  }
+//
+//}
 
 # Is Concourse ready...?
-resource "null_resource" "is-concourse-ready" {
-
-  depends_on = [
-    null_resource.install-concourse
-  ]
-
-  # https://www.terraform.io/docs/provisioners/local-exec.html
-
-  provisioner "local-exec" {
-    command = "chmod +x scripts/is_concourse_ready.sh && bash scripts/is_concourse_ready.sh"
-    environment = {
-      FOLDER    = local.folder
-      NAMESPACE = var.namespace
-    }
-  }
-
-}
+//resource "null_resource" "is-concourse-ready" {
+//
+//  depends_on = [
+//    null_resource.install-concourse
+//  ]
+//
+//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//
+//  provisioner "local-exec" {
+//    command = "chmod +x scripts/is_concourse_ready.sh && bash scripts/is_concourse_ready.sh"
+//    environment = {
+//      FOLDER    = local.folder
+//      NAMESPACE = var.namespace
+//    }
+//  }
+//
+//}
 
 # Ingress...
-resource "null_resource" "concourse-ingress" {
-
-  depends_on = [
-    null_resource.is-concourse-ready
-  ]
-
-  # https://www.terraform.io/docs/provisioners/local-exec.html
-
-  provisioner "local-exec" {
-    command = "chmod +x scripts/concourse_ingress.sh && bash scripts/concourse_ingress.sh"
-    environment = {
-      FOLDER    = local.folder
-      NAMESPACE = var.namespace
-    }
-  }
-
-}
+//resource "null_resource" "concourse-ingress" {
+//
+//  depends_on = [
+//    null_resource.is-concourse-ready
+//  ]
+//
+//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//
+//  provisioner "local-exec" {
+//    command = "chmod +x scripts/concourse_ingress.sh && bash scripts/concourse_ingress.sh"
+//    environment = {
+//      FOLDER    = local.folder
+//      NAMESPACE = var.namespace
+//    }
+//  }
+//
+//}
 
 # Using Helm install Cert-Manager...
-resource "null_resource" "install-cert-manager" {
-
-  depends_on = [
-    null_resource.is-concourse-ready
-  ]
-
-  # https://www.terraform.io/docs/provisioners/local-exec.html
-
-  provisioner "local-exec" {
-    command = "chmod +x ${path.module}/scripts/install_cert_manager.sh && bash ${path.module}/scripts/install_cert_manager.sh"
-    environment = {
-      FOLDER    = local.folder
-      NAMESPACE = "cert-manager"
-    }
-  }
-
-}
+//resource "null_resource" "install-cert-manager" {
+//
+//  depends_on = [
+//    null_resource.is-concourse-ready
+//  ]
+//
+//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//
+//  provisioner "local-exec" {
+//    command = "chmod +x ${path.module}/scripts/install_cert_manager.sh && bash ${path.module}/scripts/install_cert_manager.sh"
+//    environment = {
+//      FOLDER    = local.folder
+//      NAMESPACE = "cert-manager"
+//    }
+//  }
+//
+//}
 
 # Is Cert-Manager ready...?
-resource "null_resource" "is-cert-manager-ready" {
-
-  depends_on = [
-    null_resource.install-cert-manager
-  ]
-
-  # https://www.terraform.io/docs/provisioners/local-exec.html
-
-  provisioner "local-exec" {
-    command = "chmod +x scripts/is_cert_manager_ready.sh && bash scripts/is_cert_manager_ready.sh"
-    environment = {
-      FOLDER    = local.folder
-      NAMESPACE = "cert-manager"
-    }
-  }
-
-}
+//resource "null_resource" "is-cert-manager-ready" {
+//
+//  depends_on = [
+//    null_resource.install-cert-manager
+//  ]
+//
+//  # https://www.terraform.io/docs/provisioners/local-exec.html
+//
+//  provisioner "local-exec" {
+//    command = "chmod +x scripts/is_cert_manager_ready.sh && bash scripts/is_cert_manager_ready.sh"
+//    environment = {
+//      FOLDER    = local.folder
+//      NAMESPACE = "cert-manager"
+//    }
+//  }
+//
+//}
 
 # Configure Route53...
-module "route53" {
-
-  source = "../terraform-aws-kubernetes-cluster/modules/route53"
-
-  domain    = var.domain
-  subdomain = var.subdomain
-  public_ip = module.kubernetes-cluster.public_ip
-
-}
+//module "route53" {
+//
+//  source = "../terraform-aws-kubernetes-cluster/modules/route53"
+//
+//  domain    = var.domain
+//  subdomain = var.subdomain
+//  public_ip = module.kubernetes-cluster.public_ip
+//
+//}
